8021e824a2f0201663aa1dac149bf5a9e7cac328
lejanz <74564836+lejanz@users.noreply.github.com>
2021-08-10 16:57:54 -0700

    Wrote meat of reconcile function

--- main.py
+++ main.py
@@ -17,18 +17,25 @@ def Options(prompt, menu):
 
 def ResolveConflicts(SDE_deltas, AGOL_deltas):
     #Finds all conflicting edits. Resolves conflicts by user input. Returns revised SDE_deltas and AGOL_deltas
+
+    print('Checking for conflicts...')
+    #From here on, we will work only with global ids
     #SDE_added = GetGlobalIds(SDE_deltas['adds'])
+    #AGOL_added = GetGlobalIds(AGOL_deltas['adds'])
     SDE_updated = GetGlobalIds(SDE_deltas['updates'])
     AGOL_updated = GetGlobalIds(AGOL_deltas['updates'])
     
     #remove deletes that have already occured in destination
-    print(AGOL_deltas['deleteIds'])
-    print(SDE_deltas['deleteIds'])
+    #print(AGOL_deltas['deleteIds'])
+    #print(SDE_deltas['deleteIds'])
     SDE_deleted = set(SDE_deltas['deleteIds']).difference(AGOL_deltas['deleteIds'])
     AGOL_deleted = set(AGOL_deltas['deleteIds']).difference(SDE_deltas['deleteIds'])
-
+    
     print("SDE_deleted:", SDE_deleted)
     print("AGOL_deleted:", AGOL_deleted)
+    print("SDE_updated:", SDE_updated)
+    print("AGOL_updated:", AGOL_updated)
+
 
     #find update/delete conflictions
     AGOL_updated_SDE_deleted = AGOL_updated.intersection(SDE_deleted)
@@ -37,40 +44,95 @@ def ResolveConflicts(SDE_deltas, AGOL_deltas):
     #find update/update conflictions
     both_updated = AGOL_updated.intersection(SDE_updated)
 
-    #print("AGOL_updated_SDE_deleted:", AGOL_updated_SDE_deleted)
-    #print("SDE_updated_AGOL_deleted:", SDE_updated_AGOL_deleted)
-    #print("both_updated:", both_updated)
+    print("AGOL_updated_SDE_deleted:", AGOL_updated_SDE_deleted)
+    print("SDE_updated_AGOL_deleted:", SDE_updated_AGOL_deleted)
+    print("both_updated:", both_updated)
 
     #calculate sum of conflicts
     total_conflicts = len(AGOL_updated_SDE_deleted) + len(SDE_updated_AGOL_deleted) + len(both_updated)
 
-    #display sum of conflicts
-    #prompt user to resolve all one way, resolve manually, show more info, or cancel
-    prompt = '{} conflicts found. Choose conflict resolution:'.format(total_conflicts)
-    menu = ['Prioritize AGOL Changes', 'Prioritize SDE Changes', 'Choose for each conflict', 'More info', 'Cancel']
-    choice = Options(prompt, menu)
+    if(total_conflicts < 1):
+        print('No conflicts found.')
+    else:
+        #display sum of conflicts
+        #prompt user to resolve all one way, resolve manually, show more info, or cancel
+        prompt = '{} conflicts found. Choose conflict resolution:'.format(total_conflicts)
+        menu = ['Prioritize AGOL Changes', 'Prioritize SDE Changes', 'Choose for each conflict', 'More info', 'Cancel']
+        choice = Options(prompt, menu)
 
-    print(choice)
-
-    #if all in favor of AGOL:
-        #move AGOL_updated_SDE_deleted from AGOL_updates to AGOL_adds
-        #remove AGOL_updated_SDE_deleted from SDE_deletes
-
-        #remove SDE_updated_AGOL_deleted from SDE_updates
-
-    #same for all in favor of SDE:
-
-    #if manual: 
-        #run through all conflict lists, print out conflict, prompt to resolve in favor of AGOL or SDE
-
-        #for update/delete conflicts:
-            #if in favor of update: update -> add, delete removed
-            #if in favor of delete: update removed
-
-        #for update/update conflicts:
-            #losing update removed
-        #for ID in AGOL_updated_SDE_deleted:
+        #in update/delete conflicts, update will either become add or be removed
+        AGOL_updated -= AGOL_updated_SDE_deleted
+        SDE_updated -= SDE_updated_AGOL_deleted
         
+        #print(choice)
+        #sets to store global ids of objects being moved from updates to adds
+        AGOL_new_adds = set()
+        SDE_new_adds = set()
+
+        #if all in favor of AGOL:
+        if (choice == 1):
+            #move AGOL_updated_SDE_deleted from AGOL_updates to AGOL_adds
+            AGOL_new_adds = AGOL_updated_SDE_deleted
+            
+            #remove AGOL_updated_SDE_deleted from SDE_deletes
+            SDE_deleted -= AGOL_updated_SDE_deleted
+
+            #remove both_updated from SDE updated
+            SDE_updated -= both_updated
+
+        #same for all in favor of SDE:
+        if (choice == 2):
+            SDE_new_adds = SDE_updated_AGOL_deleted         
+            AGOL_deleted -= SDE_updated_AGOL_deleted
+            AGOL_updated -= both_updated
+
+        #if manual:
+        if (choice == 3):
+            #run through all conflict lists, print out conflict, prompt to resolve in favor of AGOL or SDE
+            menu = ['Keep update from AGOL', 'Keep delete from SDE']
+            #for update/delete conflicts:
+            for conflict in AGOL_updated_SDE_deleted:
+                prompt = 'Object "{}" was updated in AGOL and deleted in SDE. Choose:'.format(conflict)
+                choice = Options(prompt, menu)
+                
+                #if in favor of update: update -> add, delete removed
+                if (choice == 1):
+                    AGOL_new_adds.add(conflict)
+                    SDE_deleted.remove(conflict)
+                #if in favor of delete: update removed (already done above)
+
+            menu = ['Keep delete from AGOL', 'Keep update from SDE']
+            
+            for conflict in SDE_updated_AGOL_deleted:
+                prompt = 'Object "{}" was deleted in AGOL and updated in SDE. Choose:'.format(conflict)
+                choice = Options(prompt, menu)
+                
+                #if in favor of update: update -> add, delete removed
+                if (choice == 2):
+                    SDE_new_adds.add(conflict)
+                    AGOL_deleted.remove(conflict)
+                #if in favor of delete: update removed (already done above)
+                
+
+            #for update/update conflicts:
+            menu = ['Keep update from AGOL', 'Keep update from SDE']
+
+            for conflict in both_updated:
+                prompt = 'Object "{}" was updated in both AGOL and SDE. Choose:'.format(conflict)
+                choice = Options(prompt, menu)
+                #losing update removed
+                if(choice == 1):
+                    SDE_updated.remove(conflict)
+                elif(choice == 2):
+                    AGOL_updated.remove(conflict)
+                    
+
+    print("SDE_deleted:", SDE_deleted)
+    print("AGOL_deleted:", AGOL_deleted)
+    print("SDE_updated:", SDE_updated)
+    print("AGOL_updated:", AGOL_updated)
+    print("SDE_new_adds:", SDE_new_adds)
+    print("AGOL_new_adds:", AGOL_new_adds)
     
        
     return SDE_deltas, AGOL_deltas
